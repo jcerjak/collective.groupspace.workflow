f61b858b89ae604fa593fd619c99b6e221ed2704
Jure Cerjak <jcerjak@termitnjak.si>
2014-08-20 17:24:02 +0200

    fix the event handler and compatibility for different Plone versions
    This fixes what commit
    3fb83b9dfb041a12271faa4c17b142517f4984a8 was all about, so
    now the event handler is actually executed.

--- collective/groupspace/workflow/handlers.py
+++ collective/groupspace/workflow/handlers.py
@@ -4,17 +4,29 @@ Handle the setting of policies when a groupspace is added
 
 from collective.groupspace.workflow.config import CONTENT_POLICY
 from collective.groupspace.workflow.config import GROUPSPACE_POLICY
-from Products.CMFPlacefulWorkflow.PlacefulWorkflowTool import WorkflowPolicyConfig_id
+from collective.groupspace.workflow.interfaces import ILocalGroupSpaceWorkflow
+from Products.CMFPlacefulWorkflow.PlacefulWorkflowTool import \
+    WorkflowPolicyConfig_id
+from zope.component import adapter
 
+try:
+    # Plone < 4.1
+    from zope.app.container.interfaces import IObjectAddedEvent
+except ImportError:
+    # Plone >= 4.1
+    from zope.lifecycleevent.interfaces import IObjectAddedEvent
+
+
+@adapter(ILocalGroupSpaceWorkflow, IObjectAddedEvent)
 def add_local_groupspace_workflow(obj, event):
     """Apply the local workflow when a groupspace is added.
     """
     product = 'CMFPlacefulWorkflow'
     obj.manage_addProduct[product].manage_addWorkflowPolicyConfig()
     #
-    # Calling getWorkflowPolicyConfig directly is not possible because of an 
+    # Calling getWorkflowPolicyConfig directly is not possible because of an
     # explicit check in CMFPlacefulWorkflow for the permission "Manage portal",
-    # which Contributors don't have. This would only make it possible for 
+    # which Contributors don't have. This would only make it possible for
     # managers to add GroupSpaces, which doesn't make sense.
     #
     # config = placeful_workflow.getWorkflowPolicyConfig(groupspace)
@@ -24,4 +36,4 @@ def add_local_groupspace_workflow(obj, event):
     #
     config = getattr(obj.aq_explicit, WorkflowPolicyConfig_id)
     config.setPolicyIn(policy=GROUPSPACE_POLICY)
-    config.setPolicyBelow(policy=CONTENT_POLICY)       
+    config.setPolicyBelow(policy=CONTENT_POLICY)
